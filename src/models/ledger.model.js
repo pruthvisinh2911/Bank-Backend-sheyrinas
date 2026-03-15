f06fcfb2336f6263349d3ee1a98377567c79cff6
Pruthvisinh Solanki <solankipruthvi356@gmail.com>
2026-03-15 10:39:57 +0530

    creatted ledger model

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -4,10 +4,38 @@ const ledgerSchema = new mongoose.Schema({
     account:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"account",
-        required:[true,"Ledgerr must be associated with an Account"]
-
+        required:[true,"Ledgerr must be associated with an Account"],
+        index:true,
+        immutable:true,
+    }
+    amount:{
+        type:Number,
+        required:[true,"Amount is Required for creating a ledger entry"]
+        immutable:true,
+    },
+    transaction:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"transaction",
+        required:[true,"Ledger must be associated with a transaction"],
+        index:true,
+        immutable:true,
+    },
+    type:{
+        type:String,
+        enum:{
+            values:["CREDIT","DEBIT"],
+            message:"Type can be either CREDIT or DEBIT"
+        },
+        required:true,
+        immutable:true
     }
 
 },{
     timestamps:true
 })
+
+function preventLedgerModification(){
+    throw new Error("Ledger entries are immutable and cannot be modified or deleted")
+}
+
+ledgerSchema.pre('findOneAndUpdate',preventLedgerModification)
